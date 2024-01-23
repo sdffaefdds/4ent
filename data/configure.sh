c6cb8e6b92fb21da99cf91004735e0eddc7e948a
sdffaefdds <157500224+sdffaefdds@users.noreply.github.com>
2024-01-23 22:55:47 +0800

    Update configure.sh

--- data/configure.sh
+++ data/configure.sh
@@ -32,17 +32,15 @@ function install() {
     local machine=$(select_machine)
     local file_name="rathole-x86_64-unknown-linux-gnu.zip"
     local file_url="${frp_url}v${version}/${file_name}"
-    local file_dir=$(echo ${file_name} | sed 's/.zip//')
 
-    mkdir -p /tmp/$file_dir
     mkdir -p $app_path
     curl -o /tmp/${file_name} -sSL $file_url
     unzip /tmp/${file_name} -d /tmp
 
     ls -la /tmp/
-    cp -f /tmp/rathole ${app_path}/
+    cp -f /tmp/rathole /usr/src/
+    cp -f /tmp/${file_name}/rathole /usr/src/
     rm -rf /tmp/${file_name}
-    rm -rf /tmp/${file_dir}
     ls -la $app_path
 }
 
